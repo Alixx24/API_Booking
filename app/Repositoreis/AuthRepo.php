739e4c0457c6e156115669ac3beae1d0753b61d5
Ali Mohammadi <alimohammadi123450@gmail.com>
2024-07-12 18:07:38 +0330

    register Repo done

--- app/Repositoreis/AuthRepo.php
+++ app/Repositoreis/AuthRepo.php
@@ -4,6 +4,8 @@ namespace App\Repositoreis;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthRepo implements AuthRepoInterface
 {
@@ -13,5 +15,14 @@ class AuthRepo implements AuthRepoInterface
         $user = User::where('email', $request->email)->first();
 
         return $user->createToken('auth-token')->plainTextToken;
+    }
+
+    public function register(array $data)
+    {
+       return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
     }
 }
--- app/Repositoreis/AuthRepoInterface.php
+++ app/Repositoreis/AuthRepoInterface.php
@@ -3,7 +3,9 @@
 namespace App\Repositoreis;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 interface AuthRepoInterface {
     function login(Request $request);
+    function register(array $data);
 }
